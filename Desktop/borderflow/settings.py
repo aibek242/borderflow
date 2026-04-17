6f19832e988f11f4b76d30f5193f2ffad9627904
Mukash <mukash@gmail.com>
2026-04-17 21:32:31 +0500

    documents fixed2

--- Desktop/borderflow/settings.py
+++ Desktop/borderflow/settings.py
@@ -125,6 +125,8 @@ else:
     MEDIA_URL = "/media/"
     MEDIA_ROOT = BASE_DIR / "media"
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 # ================= STORAGES =================
 
 STORAGES = {
