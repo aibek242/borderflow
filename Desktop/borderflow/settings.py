d09934ae627e4db09517325f42c37ea5070ecba5
aibek007 <aibek@gmail.com>
2026-04-17 14:12:01 +0500

    fix settings.py

--- Desktop/borderflow/settings.py
+++ Desktop/borderflow/settings.py
@@ -89,4 +89,3 @@ LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
